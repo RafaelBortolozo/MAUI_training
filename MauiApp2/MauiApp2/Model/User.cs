3e9dee6f95ab41eb0fdc5d235ddbcde26e5dc8c9
RafaelBortolozo <rafaelbortolozo@outlook.com>
2022-08-10 18:00:06 -0300

    updated

---
+++ MauiApp2/MauiApp2/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2.Model
+{
+    public class User
+    {
+        public User(string name, string email, int age)
+        {
+            Id = new Guid();
+            Name = name;
+            Email = email;
+            Age = age;
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ MauiApp2/MauiApp2/View/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MauiApp2.Model;
+
+namespace MauiApp2.View;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		var users = new List<User>(){
+			new User("Rafael Bortolozo", "email1@gmail.com", 21),
+			new User("Gabriel Bortolozo", "email2@gmail.com", 35),
+			new User("Flavio Bortolozo", "email3@gmail.com", 17),
+			new User("Eduardo Bortolozo", "email4@gmail.com", 40)
+		};
+
+        this.BindingContext = users;
+
+
+
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		CounterBtn.Text = $"Clicked {count} Times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
